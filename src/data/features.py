41c539e03986e6ee4024c6b0b1610f43ad2c2d5e
mr-lovalova <47424670+mr-lovalova@users.noreply.github.com>
2023-10-31 19:59:00 +0100

    added is valid

--- src/data/features.py
+++ src/data/features.py
@@ -5,6 +5,18 @@ import pandas as pd
 from config import FEATURES, COLUMNS
 
 
+def is_valid(measurement):
+    if measurement["properties"]["parameterId"] not in FEATURES:
+        return False
+    coordinates = measurement["geometry"]["coordinates"]
+    if not coordinates:
+        return False
+    _, lat = coordinates
+    if is_greenland(lat):
+        return False
+    return True
+
+
 def get_files(path):
     return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
@@ -13,9 +25,7 @@ def parse_file(path, file, parsed):
     with open(os.path.join(path, file), "r") as f:
         for line in f:
             measurement = json.loads(line)
-            if measurement["properties"]["parameterId"] not in FEATURES or is_greenland(
-                measurement["geometry"]["coordinates"][1]
-            ):
+            if not is_valid(measurement):
                 continue
             parsed.append(parse_measurement(measurement))
 
@@ -60,7 +70,7 @@ def get_feature_data(path):
     return parsed
 
 
-def make_df(path="data/raw/metops"):
+def make_df(path):
     path = Path(path)
     parsed = get_feature_data(path)
     df = pd.DataFrame(parsed)
