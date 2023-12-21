746b84316bdbac1f8d719afa555c4f4d2f234d44
mr-lovalova <47424670+mr-lovalova@users.noreply.github.com>
2023-12-21 17:20:55 +0100

    adding notebooks for teachers

--- src/models/loops.py
+++ src/models/loops.py
@@ -46,22 +46,21 @@ def validation_loop(dataloader, model, loss_fn):
 def test_loop(dataloader, model, loss_fn):
     model.eval()
     num_batches = len(dataloader)
-    test_loss_values = []
+    municipality_errors = []
     avg_test_loss = []
     with torch.no_grad():
         for *X, y in dataloader:
             pred = model(*X)
             test_loss = loss_fn(pred, y)  # .item()
             test_loss = torch.sqrt(test_loss)
-            test_loss_values.append(torch.mean(test_loss, dim=0))
+            municipality_errors.append(torch.mean(test_loss, dim=0))
             avg_test_loss.append(torch.mean(test_loss))
     # avg_test_loss = sum(test_loss_values) / num_batches
     avg_test_loss = sum(avg_test_loss) / num_batches
-    production_err = ((torch.sum(pred) - torch.sum(y)) / torch.sum(y)) * 100
 
-    print(f"Test Error: \n Avg loss: {avg_test_loss:>8f} \n")
+    print(f"Test Error: \n Avg loss: {avg_test_loss:>8f}")
 
     return (
         avg_test_loss,
-        test_loss_values,
+        municipality_errors,
     )
